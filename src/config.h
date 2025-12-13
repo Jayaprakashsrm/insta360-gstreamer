d9499ebfe74c8a54523df39083b0eeea78e3c809
Ravi Joshi <1439011+ravijo@users.noreply.github.com>
2025-12-13 20:35:48 +0900

    Add configuration header for receiver IP and image downscale settings

---
+++ src/config.h
@@ -0,0 +1,11 @@
+#ifndef INSTA360_GSTREAMER__CONFIG_H
+#define INSTA360_GSTREAMER__CONFIG_H
+
+
+#define RECEIVER_IP "192.168.0.10"
+
+// Full resolution: 2304 x 1152
+// IMG_DOWNSCALE 4 means 1 / 4 or 25% of full resolution
+#define IMG_DOWNSCALE 4
+
+#endif // INSTA360_GSTREAMER__CONFIG_H
